afe7ead9d00e33e7c89e940c7683f5417f883817
Celovek Vodichka <celovekvodichka@gmail.com>
2024-04-12 22:38:27 +0300

    changed linter in brain_games/scripts/brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -12,14 +12,14 @@ def progression():
     global name
     print('What number is missing in the progression?')
     for i in range(3):
-        start = randint(0,100)
-        step = randint(1,100)
-        length = randint(5,15)
+        start = randint(0, 100)
+        step = randint(1, 100)
+        length = randint(5, 15)
         progression_list = []
         for _ in range(length):
             progression_list.append(str(start))
-            start+=step
-        correct_answer_position = randint(0, length-1)
+            start += step
+        correct_answer_position = randint(0, length - 1)
         correct_answer = progression_list[correct_answer_position]
         progression_list[correct_answer_position] = '..'
         if algorithm(correct_answer,
