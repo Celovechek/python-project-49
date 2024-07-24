aab73eb525b3ecc01ed298e1c263cbeaf3d6a79a
Celovek Vodichka <celovekvodichka@gmail.com>
2024-07-24 15:32:53 +0300

    create 1 function in engine

--- brain_games/scripts/logic.py
+++ brain_games/scripts/logic.py
@@ -13,24 +13,19 @@ def welcome_user():
     return NAME
 
 
-def algorithm(NAME, CORRECT_ANSWER, QUESTION_LIST):
-    print(f'Question: {" ".join(map(str, QUESTION_LIST))}')
-    USERS_ANSWER = input('Your answer: ')
-    if CORRECT_ANSWER == USERS_ANSWER:
-        print('Correct!')
-        return False
-    else:
-        print(f"'{USERS_ANSWER}' is wrong answer ;(."
-              f"Correct answer was '{CORRECT_ANSWER}'.\n"
-              f"Let's try again, {NAME}!")
-        return True
-
-
 def play(NAME, GAME_Q, game):
     print(GAME_Q)
     ROUNDS = 3
     for i in range(ROUNDS):
-        if algorithm(NAME, *game()):
+        CORRECT_ANSWER, QUESTION_LIST = game()
+        print(f'Question: {" ".join(map(str, QUESTION_LIST))}')
+        USERS_ANSWER = input('Your answer: ')
+        if CORRECT_ANSWER == USERS_ANSWER:
+            print('Correct!')
+        else:
+            print(f"'{USERS_ANSWER}' is wrong answer ;(."
+                  f"Correct answer was '{CORRECT_ANSWER}'.\n"
+                  f"Let's try again, {NAME}!")
             return
     if i == ROUNDS - 1:
         print(f'Congratulations, {NAME}!')
