83aa4de5fb6f6addfc00fd4cd80f94b9084e5775
Celovechek <160914461+Celovechek@users.noreply.github.com>
2024-08-10 16:24:08 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,17 +6,17 @@ GAME_Q = 'What number is missing in the progression?'
 
 
 def game():
-    START = randint(0, 100)
-    STEP = randint(1, 100)
-    LENGTH = randint(5, 15)
-    PROGRESSION_LIST = []
-    for _ in range(LENGTH):
-        PROGRESSION_LIST.append(str(START))
-        START += STEP
-    CORRECT_ANSWER_POSITION = randint(0, LENGTH - 1)
-    CORRECT_ANSWER = PROGRESSION_LIST[CORRECT_ANSWER_POSITION]
-    PROGRESSION_LIST[CORRECT_ANSWER_POSITION] = '..'
-    return CORRECT_ANSWER, PROGRESSION_LIST
+    start = randint(0, 100)
+    step = randint(1, 100)
+    length = randint(5, 15)
+    progression_list = []
+    for _ in range(length):
+        progression_list.append(str(start))
+        start += step
+    correct_answer_position = randint(0, length - 1)
+    correct_answer = progression_list[correct_answer_position]
+    progression_list[correct_answer_position] = '..'
+    return correct_answer, progression_list
 
 
 GAME_LIST = [GAME_Q, game]
