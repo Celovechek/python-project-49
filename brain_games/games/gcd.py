c50fa97652066c70bbd97b528bab2668514dc0f2
Celovechek <160914461+Celovechek@users.noreply.github.com>
2024-08-10 17:10:17 +0300

    Update gcd.py rename gcd calculating function

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,7 +5,7 @@ from random import randint
 GAME_RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def gcd_script(first_number, second_number):
+def calculate_gcd(first_number, second_number):
     while first_number != second_number:
         if first_number > second_number:
             first_number -= second_number
@@ -17,7 +17,7 @@ def gcd_script(first_number, second_number):
 def game():
     first_number = randint(0, 50)
     second_number = randint(0, 50)
-    correct_answer = str(gcd_script(first_number, second_number))
+    correct_answer = str(calculate_gcd(first_number, second_number))
     question_list = [first_number, second_number]
     return correct_answer, question_list
 
