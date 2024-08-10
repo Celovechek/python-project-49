8e52b024fac8c2587378eaddd722cad318c64208
Celovechek <160914461+Celovechek@users.noreply.github.com>
2024-08-10 16:23:48 +0300

    Update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,20 +5,20 @@ from random import randint
 GAME_Q = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def prime_check(NUMBER):
-    if NUMBER == 0 or NUMBER == 1:
+def prime_check(number):
+    if number == 0 or number == 1:
         return False
-    DIVIDER = 2
-    while NUMBER % DIVIDER != 0:
-        DIVIDER += 1
-    return NUMBER == DIVIDER
+    divider = 2
+    while number % divider != 0:
+        divider += 1
+    return number == divider
 
 
 def game():
-    NUMBER = randint(0, 100)
-    CORRECT_ANSWER = "yes" if prime_check(NUMBER) else "no"
-    QUESTION_LIST = [NUMBER]
-    return CORRECT_ANSWER, QUESTION_LIST
+    number = randint(0, 100)
+    correct_answer = "yes" if prime_check(number) else "no"
+    question_list = [number]
+    return correct_answer, question_list
 
 
 GAME_LIST = [GAME_Q, game]
