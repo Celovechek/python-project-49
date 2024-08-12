d44fd0518255a2531b21ff690ec5e2cbb68f7819
Celovechek <160914461+Celovechek@users.noreply.github.com>
2024-08-12 19:48:55 +0300

    Update calc.py remove eval

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from random import randint, choice
 
-
 GAME_RULES = 'What is the result of the expression?'
 
 
@@ -11,8 +10,14 @@ def question_and_answer():
     first_number = randint(start, end)
     second_number = randint(start, end)
     operation = choice(['+', '-', '*'])
-    correct_answer = str(eval(str(first_number) + operation
-                              + str(second_number)))
+
+    if operation == '+':
+        correct_answer = str(first_number + second_number)
+    elif operation == '-':
+        correct_answer = str(first_number - second_number)
+    elif operation == '*':
+        correct_answer = str(first_number * second_number)
+
     question_list = [first_number, operation, second_number]
     return correct_answer, question_list
 
