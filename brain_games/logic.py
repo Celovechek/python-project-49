d980b89778af17be13f9a291316352940659d24c
Celovechek <160914461+Celovechek@users.noreply.github.com>
2024-08-12 19:21:46 +0300

    Update logic.py change names and rounds

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -1,22 +1,21 @@
 import prompt
 
 
-def play(GAME_RULES, question_and_answer):
+def play(game_rules, question_and_answer, ROUNDS=3):
     print('Welcome to the Brain Games!')
-    NAME = prompt.string("May I have your name? ")
-    print(f"Hello, {NAME}!")
-    print(GAME_RULES)
-    ROUNDS = 3
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print(game_rules)
     for i in range(ROUNDS):
-        CORRECT_ANSWER, QUESTION_LIST = question_and_answer()
-        print(f'Question: {" ".join(map(str, QUESTION_LIST))}')
-        USERS_ANSWER = input('Your answer: ')
-        if CORRECT_ANSWER == USERS_ANSWER:
+        correct_answer, question_list = question_and_answer()
+        print(f'Question: {" ".join(map(str, question_list))}')
+        users_answer = input('Your answer: ')
+        if correct_answer == users_answer:
             print('Correct!')
         else:
-            print(f"'{USERS_ANSWER}' is wrong answer ;(."
-                  f"Correct answer was '{CORRECT_ANSWER}'.\n"
-                  f"Let's try again, {NAME}!")
+            print(f"'{users_answer}' is wrong answer ;(."
+                  f"Correct answer was '{correct_answer}'.\n"
+                  f"Let's try again, {name}!")
             return
     if i == ROUNDS - 1:
-        print(f'Congratulations, {NAME}!')
+        print(f'Congratulations, {name}!')
